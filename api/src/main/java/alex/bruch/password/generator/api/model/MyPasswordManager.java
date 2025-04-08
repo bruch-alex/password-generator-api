cd2f94f0cf3d63ee8793d09a3555ecf7c864c666
bruch-alex <alex.bruch@tutanota.com>
2025-04-08 14:50:34 +0200

    sets are replaced with lists

--- api/src/main/java/alex/bruch/password/generator/api/model/MyPasswordManager.java
+++ api/src/main/java/alex/bruch/password/generator/api/model/MyPasswordManager.java
@@ -10,9 +10,9 @@ import java.util.Set;
 @Component
 public class MyPasswordManager {
     //CharSets
-    private final Set<Integer> lowerCaseDecimal = Set.of(97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122);
-    private final Set<Integer> upperCaseDecimal = Set.of(65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90);
-    private final Set<Integer> digitsDecimal = Set.of(48, 49, 50, 51, 52, 53, 54, 55, 56, 57);
+    private final List<Integer> lowerCaseDecimal = List.of(97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122);
+    private final List<Integer> upperCaseDecimal = List.of(65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90);
+    private final List<Integer> digitsDecimal = List.of(48, 49, 50, 51, 52, 53, 54, 55, 56, 57);
     // Generator properties
     private final List<Integer> finalSet = new ArrayList<>();
     private final Random random = new Random();
